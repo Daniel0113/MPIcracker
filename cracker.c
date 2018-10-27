1d45e6077c398d76cbe12af9dd8642a8ec768a5b
Daniel0113 <daniel.febles97@gmail.com>
2018-10-27 17:04:01 -0400

    added collab file

--- cracker.c
+++ cracker.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <mpi.h>
 
 unsigned long hash(unsigned char *str)
@@ -13,14 +14,38 @@ unsigned long hash(unsigned char *str)
     return hash;
 } // http://www.cse.yorku.ca/~oz/hash.html
 
+int comparePasswords(unsigned char *input, unsigned long passwordHash)
+{
+  return (hash(input) == passwordHash);
+}
+
+const char *passList[10] = {"password", "stuff", "door", "lamp", "secure", "12345", "mpi", "onomatopoeia", "thicc", "phone"};
+
+
 int main(int argc, char *argv[])
 {
-	int rank, size, len;
+	const int MAX_PASS_LEN = 10;
+	int rank, size, len, elementsPerProcess;
 	char name[MPI_MAX_PROCESSOR_NAME];
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Get_processor_name(name, &len);
+	printf("Stuff\n");
+	elementsPerProcess = size / (sizeof(passList) / sizeof(passList[0]));
+	char **subList;
+	int i;
+	
+	subList = malloc(elementsPerProcess * sizeof(char*));
+	for (i = 0; i < elementsPerProcess; i++)
+  {
+    subList[i] = malloc((MAX_PASS_LEN + 1) * sizeof(char));
+  }
+  MPI_Scatter(passList, elementsPerProcess, MPI_CHAR, subList, elementsPerProcess, MPI_CHAR, 0, MPI_COMM_WORLD);
+  for(i = 0; i < 2; i++)
+  {
+	printf("I am node %d getting %s\n", rank, *subList[i]);
+  } // TEST FOR LOOP
 	printf ("Hello world! I'm %d of %d on %s\n", rank, size, name);
 	MPI_Finalize();
 	exit(0);
