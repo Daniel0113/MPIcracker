ead350640b276ef7805cfdc48bfc9441735e5289
Zachary Rivera <zrivera129@gmail.com>
2018-11-11 19:37:18 -0500

    Refactored variables; Allow any sized file input

--- cracker.c
+++ cracker.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <mpi.h>
 #include <unistd.h>
+#define MASTER 0
 
 unsigned long hash(unsigned char *str) {
     unsigned long hash = 5381;
@@ -21,7 +22,7 @@ int comparePasswords(unsigned char *input, unsigned long passwordHash) {
 
 char **passList, **subList;
 unsigned long hashToFind;
-int i, j, rank, size, elementsPerProcess, arraysize, MAX_PASS_LEN;
+int i, j, rank, size, elementsPerProcess, arraySize, MAX_PASS_LEN;
 MPI_Status status;
 
 void init_system(int argc, char *argv[]);
@@ -49,51 +50,53 @@ void init_system(int argc, char *argv[]) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    FILE *inp;
+    FILE *input;
     //read file and get array size
     hashToFind = strtoul(argv[2], NULL, 0);
-    inp = fopen(argv[1], "r");
+    input = fopen(argv[1], "r");
     int i = 0;
     MAX_PASS_LEN = 0;
     while (1) {
-        char r = (char) fgetc(inp);
-        int k = 0;
-        while (r != '\n' && !feof(inp)) {    //read till \n or EOF
-            k++;
-            r = (char) fgetc(inp);
+        char ch = (char) fgetc(input);
+        j = 0;
+        while (ch != '\n' && !feof(input)) {    //read till \n or EOF
+            j++;
+            ch = (char) fgetc(input);
         }
-        if (feof(inp)) {        //check again for EOF
+        if (feof(input)) {        //check again for EOF
             break;
         }
-        if (k > MAX_PASS_LEN) MAX_PASS_LEN = k;
+        if (j > MAX_PASS_LEN) MAX_PASS_LEN = j;
         i++;
     }
-    fclose(inp);
+    fclose(input);
 
-    arraysize = i;
-    if (arraysize % size != 0) {
+    arraySize = i;
+/*    if (arraySize % size != 0) {
         printf("File must be divisible by nodes\n");
         MPI_Abort(MPI_COMM_WORLD, 2);
+    }*/
+    elementsPerProcess = arraySize / size;
+    if (rank == size-1){ // Last node
+        elementsPerProcess += arraySize % size;
     }
-    elementsPerProcess = arraysize / size;
-
-    if (rank == 0) {
-        passList = (char **) malloc(sizeof(char *) * arraysize);
-        for (i = 0; i < arraysize; i++) {
+    if (rank == MASTER) {
+        passList = (char **) malloc(sizeof(char *) * arraySize);
+        for (i = 0; i < arraySize; i++) {
             passList[i] = (char *) malloc((MAX_PASS_LEN) * sizeof(char));
             memset(passList[i], '\0', (MAX_PASS_LEN) * sizeof(char));
         }
-        i = 0;
+        i = 0; // reset variable to 0
         // read file and save to array
-        inp = fopen(argv[1], "r");
+        input = fopen(argv[1], "r");
         while (1) {
-            char r = (char) fgetc(inp);
-            int k = 0;
-            while (r != '\n' && !feof(inp)) {    //read till \n or EOF
-                passList[i][k++] = r;            //store in array
-                r = (char) fgetc(inp);
+            char ch = (char) fgetc(input);
+            j = 0;
+            while (ch != '\n' && !feof(input)) {    //read till \n or EOF
+                passList[i][j++] = ch;            //store in array
+                ch = (char) fgetc(input);
             }
-            if (feof(inp)) {        //check again for EOF
+            if (feof(input)) {        //check again for EOF
                 break;
             }
             i++;
@@ -110,14 +113,17 @@ void root_task() {
     j = elementsPerProcess;
     int k = j;
     for (i = 1; i < size; i++) {
+        if (i == size-1){ // Last node
+            elementsPerProcess += arraySize % size;
+        }
         for (; j < k + elementsPerProcess; j++) {
-            MPI_Send(passList[j], strlen(passList[j]), MPI_CHAR, i, 0, MPI_COMM_WORLD);
+            MPI_Send(passList[j], (int) strlen(passList[j]), MPI_CHAR, i, 0, MPI_COMM_WORLD);
         }
         k = j;
     }
 
     for (i = 0; i < elementsPerProcess; i++) {
-        if(comparePasswords(passList[i], hashToFind)){
+        if(comparePasswords((unsigned char*) passList[i], hashToFind)){
             printf("I am node %d. I have %s\n", rank, passList[i]);
         }
     }
@@ -125,10 +131,10 @@ void root_task() {
 
 void worker_task() {
     for (i = 0; i < elementsPerProcess; i++) {
-        MPI_Recv(subList[i], MAX_PASS_LEN, MPI_CHAR, 0, 0, MPI_COMM_WORLD, &status);
+        MPI_Recv(subList[i], MAX_PASS_LEN, MPI_CHAR, MASTER, 0, MPI_COMM_WORLD, &status);
     }
     for (i = 0; i < elementsPerProcess; i++) {
-        if(comparePasswords(subList[i], hashToFind)){
+        if(comparePasswords((unsigned char*) subList[i], hashToFind)){
             printf("I am node %d. I have %s\n", rank, subList[i]);
         }
     }
